fcfcf215db7b64ff56c681d0dd01b46a13c964f6
Anna Preuss <anna.preuss@stud.fh-campuswien.ac.at>
2022-09-28 23:32:00 +0200

    Task 2 entries done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -13,6 +13,16 @@ public class    App {
     //todo Task 2
     public void helloRobot(){
         // input your solution here
+        System.out.println("0123456789012345678901      ");
+        System.out.println("         __                 ");
+        System.out.println(" _(\\    |@@|               ");
+        System.out.println("(__/\\__ \\--/ __           ");
+        System.out.println("   \\___|----|  |   __      ");
+        System.out.println("       \\ }{ /\\ )_ / _\\   ");
+        System.out.println("       /\\__/\\ \\__O (__   ");
+        System.out.println("      (--/\\--)    \\__/    ");
+        System.out.println("      _)(  )(_              ");
+        System.out.println("     `---''---`             ");
     }
 
     //todo Task 3
